f9f80e96f5ab7ae4c16a39fc6da91eea78fd9233
Davide Pizzolato <pizzolato.davide00@gmail.com>
2021-08-08 15:55:53 +0200

    Create frontend project

--- just4fun-fe/src/app/app.module.ts
+++ just4fun-fe/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
