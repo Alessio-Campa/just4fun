df0b8e417844b70cfe8f7cc7fce428ab6d3537c9
Alessio-Campa <878170@stud.unive.it>
2021-08-19 15:41:53 +0200

    Migliorie grafiche random

--- just4fun-fe/src/app/profile/profile.component.ts
+++ just4fun-fe/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User, UserService} from "../services/user.service";
+import {Router} from "@angular/router";
+import {Match, MatchService} from "../services/match.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  ongoingMatches: Match[];
+
+  constructor(private userService: UserService, private router: Router, private matchService: MatchService) {
+    if(!this.userService.isLoggedIn)
+      router.navigate(['/login']);
+  }
 
   ngOnInit(): void {
+    this.userService.get_user_by_mail( this.userService.email ).subscribe(data => {
+      this.user = data;
+    });
+    this.matchService.getUserMatches( this.userService.email, false ).subscribe( data => {
+      this.ongoingMatches = data;
+    })
   }
+
 
 }
